785d6d2d0624c551359e51ba9f098c6b7a2e722b
gargenal1 <gabriel.argenal@gmail.com>
2020-01-01 13:30:22 -0600

    version 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,16 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     module: {
         rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+              },
             {
                 test: /\.html$/,
                 use: [
@@ -12,12 +20,32 @@ module.exports = {
                     }
                 ]
             },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+              },
+              {
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+              },
         ]    
     },
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/index.html",
             filename: "./index.html"
+        }),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackoptions.output
+            // both options are optional
+            filename: "[name].css",
+            chunkFilename: "[id].css"
         }),
     ]
 }
